37e75db2cf85b922d88aca65ff04f37c6cbc1acc
Eric Snow <ericsnowcurrently@gmail.com>
2017-08-10 01:56:02 +0000

    (WIP) Support using numpy for efficiency.

--- mandelbrot/__main__.py
+++ mandelbrot/__main__.py
@@ -73,3 +73,36 @@ def main(radius=1.5, center=Point2D(-0.75, 0), steps=None, *,
 if __name__ == '__main__':
     args = parse_args()
     main(**vars(args))
+
+
+"""
+def mandelbrot_np(radius=1.5, centera=-0.75, centerb=0, scale=400):
+    import numpy as np
+    import matplotlib
+    import matplotlib.pyplot as plt
+
+    xmin, xmax, xn = centera - radius, centera + radius, scale + 1
+    ymin, ymax, yn = centerb - radius, centerb + radius, scale + 1
+    X = np.linspace(xmin, xmax, xn, dtype=np.float32)
+    Y = np.linspace(ymin, ymax, yn, dtype=np.float32)
+    C = X + Y[:, None]*1j
+    N = np.zeros(C.shape, dtype=int)
+
+    candidates = _iter_c5(radius, centera, centerb, scale)
+    for c, i in _mandelbrot5(candidates, MAX_ITER):
+        N[c] = i
+
+    dpi = 72
+    width = 10
+    height = 10*yn/xn
+    fig = plt.figure(figsize=(width, height), dpi=dpi)
+    ax = fig.add_axes([0.0, 0.0, 1.0, 1.0], frameon=False, aspect=1)
+    # Shaded rendering
+    #light = matplotlib.colors.LightSource(azdeg=315, altdeg=10)
+    #M = light.shade(M, cmap=plt.cm.hot, vert_exag=1.5,
+    #                norm=colors.PowerNorm(0.3), blend_mode='hsv')
+    plt.imshow(M, extent=[xmin, xmax, ymin, ymax], interpolation="bicubic")
+    ax.set_xticks([])
+    ax.set_yticks([])
+    plt.show()
+"""
